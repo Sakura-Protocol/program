118e35e6762c54b9cfaed2ebaf71f6f788ebf63f
ProPratik1405 <sariapratik@gmail.com>
2022-08-17 00:20:23 +0545

    Init

---
+++ programs/solstore/src/lib.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod solstore {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+
+    // Create an Admin Account that's going to store the Admin Config Info + the funds gets comming here
+
+    // Allow Admin Account to create Coupons and Products
+
+    // Allow Admin to create Loyalty Points
+
+    // Allow All the User to create GiftCards
+
+    // Create a Product Account which is gonna be gated and is managed by the admin
+    
+    // NFT Coupons are managed against the Product
+
+    // NFT Gift Cards are managed against the Product
+
+    // There is going to be a pay function that is going to swap from orca and tranfer the Funds to the merchant account
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
